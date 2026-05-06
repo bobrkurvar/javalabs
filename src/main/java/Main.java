9ccd941409588b67770cf4eb021283c6ad825efb
bobrkurvar <andrey.bogdanov2005@mail.ru>
2026-05-06 12:04:19 +0300

    Написал Main, запускающий проверку

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
 public class Main {
 
+    public static void main(String[] args) {
+        int iterations = 10000;
+
+        System.out.println("=== Запуск тестирования коллекций на " + iterations + " элементов ===\n");
+
+        CollectionBenchmark benchmark = new CollectionBenchmark(iterations);
+        benchmark.runAll();
+    }
 }
